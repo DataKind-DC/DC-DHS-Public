483bc7b626d20895da04c53970b188a804772a0e
Richard Carder <rcarder@wpfund.org>
2021-09-29 18:54:55 -0400

    Disaggregate by Gender

--- Scripts/DataAggregation.R
+++ Scripts/DataAggregation.R
@@ -8,18 +8,25 @@
 library(googlesheets4)
 library(rnoaa)
 library(tidyverse)
+library(lubridate)
+library(GGally)
 
 
 ##Load Raw Data
 weatherData<-read_sheet("https://docs.google.com/spreadsheets/d/1w8WCWvveYq2HppELyw3jebhPEO9kGZNhj8GBnegZlbM/edit#gid=1585438251")
-stayData<-read_sheet("https://docs.google.com/spreadsheets/d/1m4zCOrHzPWb_GsaEr9FL30VYUUgUZbRRfWU_sMzaQK8/edit#gid=872071627")
+#stayData<-read_sheet("https://docs.google.com/spreadsheets/d/1m4zCOrHzPWb_GsaEr9FL30VYUUgUZbRRfWU_sMzaQK8/edit#gid=872071627")
+stayDataGender<-read_sheet("https://docs.google.com/spreadsheets/d/1LfFB4Xcv5Io0Q8SmezZohf0J01qeqT2eyq9zsUdoBVs/edit#gid=257172774")
+  
+stayData<-stayDataGender
 
+  
 unique(stayData$name)
 n_distinct(stayData$client_id)
 
-
+table<-as.data.frame(table(stayData$end))
+unique(table$Freq)
 ##Some date cleaning; filling in NAs in exit date with the day date was pulled for time calculations
-datePulled<-"2021-06-11"
+datePulled<-as.Date("2021-09-01")
 stayData$end[is.na(stayData$end)]<-datePulled
 stayData$end[stayData$end=='']<-datePulled
 stayData$start<-as.Date(stayData$start)
@@ -52,7 +59,7 @@ for (i in Dates){
   
 temp<-stayData%>%
   mutate(inRange=ifelse(start<=i&end>=i,1,0))%>%
-  group_by(name)%>%
+  group_by(gender)%>%
   summarize(Total=sum(inRange))%>%
   mutate(date=as.Date(as.POSIXct.Date(i)))
 
@@ -62,25 +69,28 @@ DateCount<-bind_rows(DateCount,temp)
 
 
 TotalDateCount<-DateCount%>%
-  group_by(date)%>%
-  summarise(Total=sum(Total))%>%
-  mutate(dayWeek=wday(date),
-         month=month(date),
-         day=day(date),
-         year=year(date))%>%
+  pivot_wider(names_from = gender,values_from = Total)%>%
+  mutate(dayWeek=factor(wday(date)),
+         month=factor(month(date)),
+         day=factor(day(date)),
+         year=factor(year(date)))%>%
   left_join(weatherDataClean)%>%
-  mutate(FreezingAtEntry=if_else(MinTempF<=32,1,0))
+  mutate(FreezingAtEntry=if_else(MinTempF<=32,1,0))%>%
+  filter(date<="2021-09-02")
 
 
 
-ggplot(TotalDateCount,aes(x=date,y=Total))+geom_line()
-ggplot(TotalDateCount,aes(x=MinTempF,y=Total))+geom_point()
+ggplot(TotalDateCount,aes(x=date,y=Male))+geom_line()
+ggplot(TotalDateCount,aes(x=date,y=Female))+geom_line()
+ggplot(TotalDateCount,aes(x=MinTempF,y=Male))+geom_point()
 ggplot(TotalDateCount,aes(x=dayWeek,group=dayWeek,y=Total))+geom_boxplot()
+
+ggpairs(TotalDateCount[,c(2,4,9:17)])
 
 
 ##write output data locally to different dir than repo clone
 setwd("C:/Users/rcarder/Documents/dev/DHS")
-write.csv(TotalDateCount,"InShelterPerDay.csv",row.names = FALSE)
+write.csv(TotalDateCount,"InShelterPerDayGender.csv",row.names = FALSE)
 
 ##The output of this script has been added to the public datacorps Google Drive folder for modeling by volunteers
 
